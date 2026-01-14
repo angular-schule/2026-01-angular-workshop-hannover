e31c33b43b6b196ac3f72139e356631388a0566e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2026-01-14 14:30:37 +0100

    Parent-Child Component

--- book-rating/src/app/app.ts
+++ book-rating/src/app/app.ts
@@ -1,10 +1,11 @@
 
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardPage } from "./books/dashboard-page/dashboard-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DashboardPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
