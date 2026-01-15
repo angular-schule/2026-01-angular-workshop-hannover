c9f963920be1f050707a32b300b61f725bae544f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2026-01-15 10:44:23 +0100

    working with signals

--- book-rating/src/app/books/dashboard-page/dashboard-page.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-page',
   imports: [],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush // weniger Prüfungen als vorher
 })
 export class DashboardPage {
+
+  readonly books = signal(['Angular', 'jQuery', 'React']);
+
+  constructor() {
+    setTimeout(() => this.books.update(x => {
+      const newX = [...x];
+      newX[1] = 'Vuejs';
+      return newX;
+    }), 2000)
+  }
 
 }
