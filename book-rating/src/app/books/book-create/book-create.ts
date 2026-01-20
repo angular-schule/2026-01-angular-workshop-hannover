e897c12e30fbc8590d46a1e91ef1754206707848
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2026-01-20 09:40:32 +0100

    forms: schema

--- book-rating/src/app/books/book-create/book-create.ts
+++ book-rating/src/app/books/book-create/book-create.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Book } from '../shared/book';
-import { form } from '@angular/forms/signals';
+import { form, max, maxLength, min, minLength, required, schema, validate } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-book-create',
@@ -17,5 +17,26 @@ export class BookCreate {
     rating: 1
   });
 
-  protected readonly bookForm = form(this.#formData);
+  protected readonly bookForm = form(this.#formData, schema(path => {
+
+    required(path.isbn, { message: 'Die ISBN muss angegeben werden.' });
+    required(path.title, { message: 'Der Titel muss angegeben werden.' });
+    required(path.rating, { message: 'Die Bewertung muss angegeben werden.' });
+
+    minLength(path.isbn, 8, { message: 'Die ISBN muss min. 8 Zeichen lang sein.' });
+    maxLength(path.isbn, 15, { message: 'Die ISBN darf max. 15 Zeichen lang sein.' });
+
+    min(path.rating, 1, { message: 'Die Bewertung muss min. 1 sein.' });
+    max(path.rating, 5, { message: 'Die Bewertung muss max. 5 sein.' });
+
+    validate(path.isbn, ctx => {
+      if (!ctx.value().startsWith('978')) {
+        return {
+          kind: 'isbnPrefix',
+          message: 'Eine ISBN muss mit 978 beginnen.'
+        }
+      }
+      return undefined;
+    });
+  }));
 }
